71ee398b38d3e69e8c53e5c736a65fabc8e9f7a0
philtabor <ptabor@gmail.com>
2024-05-20 09:35:07 -0400

    update package requirements

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "numpy==1.21.*",
-        "torch==1.11.*",
+        "numpy>=1.21.*",
+        "torch>=1.11.*",
         "gym==0.26.*",
         "gym[box2d]",
         "atari-py==0.2.6",
