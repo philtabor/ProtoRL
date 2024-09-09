00024ab790719aee20f58d48c48005468d3c73b4
philtabor <ptabor@gmail.com>
2024-09-09 09:28:45 -0400

    add support for 3rd party gym envs

--- protorl/wrappers/common.py
+++ protorl/wrappers/common.py
@@ -1,10 +1,21 @@
+import importlib
 import gymnasium as gym
 from gymnasium.wrappers import FrameStack
 from protorl.wrappers.single_threaded import BatchDimensionWrapper
 
 
-def make_env(env_name, use_atari=False, repeat=4, no_ops=0, **kwargs):
-    env = gym.make(env_name, **kwargs)
+def make_env(env_name, use_atari=False, repeat=4,
+             no_ops=0, package_to_import=None, **kwargs):
+
+    try:
+        env = gym.make(env_name, **kwargs)
+    except (gym.error.Error, ImportError) as e:
+        if package_to_import:
+            try:
+                importlib.import_module(package_to_import)
+                env = gym.make(env_name, **kwargs)
+            except ImportError:
+                raise ImportError(f"Could not import {package_to_import}. Please ensure it's installed.")
 
     if use_atari:
         env = gym.wrappers.AtariPreprocessing(env, noop_max=no_ops, scale_obs=True)
