33360d3572b230fa38838723bb718f789d4ef705
Phil Tabor <ptabor@gmail.com>
2024-12-31 14:12:00 -0500

    performance improvements

--- protorl/learner/ppo.py
+++ protorl/learner/ppo.py
@@ -1,6 +1,6 @@
 import torch as T
 from protorl.learner.base import Learner
-from protorl.utils.common import calc_adv_and_returns
+from protorl.utils.common import calc_adv_and_returns, swap_and_flatten
 
 
 class PPOLearner(Learner):
@@ -39,17 +39,24 @@ class PPOLearner(Learner):
         self.actor_optimizer.load_state_dict(checkpoint['actor_optimizer_state_dict'])
         self.critic_optimizer.load_state_dict(checkpoint['critic_optimizer_state_dict'])
 
-    def update(self, transitions, batches):
-        s, a, r, s_, d, _ = transitions
+    def update(self, transitions, batches, ext_adv, ext_ret):
+        s, a, r, d, lp, v, v_ = transitions
 
-        with T.no_grad():
-            values = self.critic(s).squeeze()
-            values_ = self.critic(s_).squeeze()
+        if ext_adv is None and ext_ret is None:
+            ext_adv, ext_ret = calc_adv_and_returns(v, v_, r, d)
+            ext_adv = swap_and_flatten(ext_adv).squeeze()
+            ext_ret = swap_and_flatten(ext_ret).squeeze()
 
-        adv, returns = calc_adv_and_returns(values, values_, r, d)
+        s = swap_and_flatten(s)
+        a = swap_and_flatten(a)
+        lp = swap_and_flatten(lp)
 
         for batch in batches:
-            indices, states, actions, _, _, _, old_probs = batch
+            indices = batch[0].to(T.int)
+            states = s[indices]
+            actions = a[indices]
+            old_probs = lp[indices]
+
             if self.action_type == 'continuous':
                 alpha, beta = self.actor(states)
                 _, new_probs, entropy = self.policy(alpha, beta,
@@ -58,37 +65,37 @@ class PPOLearner(Learner):
                 last_dim = int(len(new_probs.shape) - 1)
                 prob_ratio = T.exp(
                     new_probs.sum(last_dim, keepdims=True) -
-                    old_probs.sum(last_dim, keepdims=True))
-
-                entropy = entropy.sum(last_dim, keepdims=True)
+                    old_probs.sum(last_dim, keepdims=True)).squeeze()
+                entropy = entropy.sum(last_dim, keepdims=True).squeeze()
 
             elif self.action_type == 'discrete':
                 probs = self.actor(states)
                 _, new_probs, entropy = self.policy(probs,
-                                                    old_action=actions,
+                                                    old_action=actions.squeeze(),
                                                     with_entropy=True)
-                prob_ratio = T.exp(new_probs - old_probs)
-            a = adv[indices].view(prob_ratio.shape)
-            weighted_probs = a * prob_ratio
+                prob_ratio = T.exp(new_probs - old_probs.squeeze())
+            adv = ext_adv[indices]
+            adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+            weighted_probs = adv * prob_ratio
             weighted_clipped_probs = T.clamp(
-                    prob_ratio, 1-self.policy_clip, 1+self.policy_clip) * a
+                    prob_ratio, 1-self.policy_clip, 1+self.policy_clip) * adv
 
             actor_loss = -T.min(weighted_probs,
                                 weighted_clipped_probs)
 
             actor_loss -= self.entropy_coefficient * entropy
-
             self.actor_optimizer.zero_grad()
             actor_loss.mean().backward()
             T.nn.utils.clip_grad_norm_(self.actor.parameters(), 0.5)
             self.actor_optimizer.step()
 
             critic_value = self.critic(states).squeeze()
-            critic_loss = (critic_value - returns[indices].squeeze()).\
+            critic_loss = (critic_value - ext_ret[indices].squeeze()).\
                 pow(2).mean()
             self.critic_optimizer.zero_grad()
             critic_loss.backward()
             self.critic_optimizer.step()
+        return ext_adv, ext_ret
 
     def anneal_policy_clip(self, n_ep, max_ep):
         self.policy_clip = self.policy_clip_start * (1 - n_ep / max_ep)
